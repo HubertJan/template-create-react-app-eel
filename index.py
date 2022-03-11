795e1fa08bb84df752122e8cab2e24375ea529f0
HubertJan <70658889+HubertJan@users.noreply.github.com>
2022-03-11 15:13:00 +0100

    working, without typescript

--- index.py
+++ index.py
@@ -9,9 +9,9 @@ def hello():
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == '--develop':
+    if len(sys.argv) >= 2 and sys.argv[1] == '--develop':
         eel.init('client')
         eel.start({"port": 3000}, host="localhost", port=8888)
     else:
         eel.init('build')
-        eel.start('index.html')
+        eel.start('index.html', host="localhost", port=8888)
